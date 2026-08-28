d1baa58384bde0f2889e52ea1b4a7a8ef631863a
Ismail <ismail.benikabour7@gmail.com>
2026-08-28 16:03:59 +0200

    Fix: Eliminada importacion innecesaria de tailwind en config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://unimatches.netlify.app', // Tu URL de Netlify aquí
-  integrations: [tailwind(), sitemap()],
+  site: 'https://unimatches.netlify.app',
+  integrations: [sitemap()],
 });
