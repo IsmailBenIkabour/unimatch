d5b96ce3727099adad8cf7b2c3fcec0eaccc42b7
Ismail <ismail.benikabour7@gmail.com>
2026-08-28 16:01:20 +0200

    Fase 6: Anadido plugin de Sitemap automatico para SEO en Google

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  site: 'https://unimatches.netlify.app', // Tu URL de Netlify aquí
+  integrations: [tailwind(), sitemap()],
 });
