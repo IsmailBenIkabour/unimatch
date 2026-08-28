504c051a526ccee364bcb21a762dedb2bc917559
Ismail <ismail.benikabour7@gmail.com>
2026-08-28 18:01:50 +0200

    Chore: Actualizado sitio web y canónicas a unimatchcat.netlify.app

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://unimatches.netlify.app',
+  site: 'https://unimatchcat.netlify.app',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
